d549ecdbc46878828fa2edad36db3c3996e4d4bf
HarounHK <harounkassouri368@gmail.com>
2024-12-31 23:06:36 +0000

    Added .env file and updated settings

--- BookNest/BookNest/settings.py
+++ BookNest/BookNest/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,7 @@ SECRET_KEY = 'django-insecure-y522-w%u1sftgt=26l)4=e35xvfm6-1b$bh^#mrh*rkw=v)92j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
